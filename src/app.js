8c985fcfb73d10efb3516bd868dc8de25367f11f
dedalovitor <dedalovitor@gmail.com>
2022-11-05 09:38:53 +0000

    uploading my code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoum = ["A", "The"];
+  let subject = ["jogger", "racoon", "driver", "dog", "comedian", "pinecone"];
+  let actions = ["took", "threw", "yellew at", "stole", "bit"];
+  let possession = ["my homework", "my toe", "my car", "my shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proIndx = Math.floor(Math.random() * pronoum.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actIndx = Math.floor(Math.random() * actions.length);
+  let possIndx = Math.floor(Math.random() * possession.length);
+  let wherIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoum[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    actions[actIndx] +
+    " " +
+    possession[possIndx] +
+    " " +
+    where[wherIndx] +
+    " "
+  );
 };
